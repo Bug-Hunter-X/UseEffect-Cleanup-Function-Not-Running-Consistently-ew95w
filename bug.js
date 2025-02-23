7b69507c358dbfe189c8ccb10a61b1c4c2f23591
BugHunter-X <askzenai@gmail.com>
2025-02-23 07:40:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,25 @@
+```javascript
+import React, { useState, useEffect } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This effect runs on every render
+    console.log('Effect ran!');
+    return () => {
+      // Cleanup function
+      console.log('Cleanup function ran!');
+    };
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
